15310c283696b0f5332841be97630a46bc11a5b7
José Carlos Siqueira <josecarlos.siqueira76@gmail.com>
2018-06-20 21:10:39 -0300

    Pimeiro commit

--- src/main/java/MegaSena/LeitorMegaSena/Teste.java
+++ src/main/java/MegaSena/LeitorMegaSena/Teste.java
@@ -1,6 +1,9 @@
 package MegaSena.LeitorMegaSena;
 
+import java.io.DataInputStream;
 import java.io.File;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
@@ -12,17 +15,17 @@ public class Teste {
 	public static void main(String[] args) {
 		
 	String exePath = "C:\\Users\\José Carlos\\Documents\\"
-			+ "IEDriverServer.exe";
+		+ "IEDriverServer.exe";
 	InternetExplorerDriverService.Builder serviceBuilder = new InternetExplorerDriverService.Builder();
 	serviceBuilder.usingAnyFreePort(); // This specifies that sever can pick any available free port to start
 	serviceBuilder.usingDriverExecutable(new File(exePath)); //Tell it where you server exe is
-	//serviceBuilder.withLogLevel(InternetExplorerDriverLogLevel.TRACE); //Specifies the log level of the server
-	//serviceBuilder.withLogFile(new File("C:\\Users\\abc\\Documents\\logFile.txt")); //Specify the log file. Change it based on your system
+	serviceBuilder.withLogLevel(InternetExplorerDriverLogLevel.TRACE); //Specifies the log level of the server
+	serviceBuilder.withLogFile(new File("C:\\Users\\abc\\Documents\\logFile.txt")); //Specify the log file. Change it based on your system
 	InternetExplorerDriverService service = serviceBuilder.build(); //Create a driver service and pass it to Internet explorer driver instance
 	InternetExplorerDriver driver = new InternetExplorerDriver(service);
-	//driver.get("C:\\Users\\José Carlos\\Downloads\\D_MEGA.HTM");
+	driver.get("C:\\Users\\José Carlos\\Downloads\\D_MEGA.HTM");
+		
 		
 	}
-	
 
 }
